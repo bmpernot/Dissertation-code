7f33c325435291a6200627484f49f6daf725f9d4
bmpernot <61884694+bmpernot@users.noreply.github.com>
2022-04-27 10:32:14 +0100

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,9 @@
-<footer class="mt-4 text-center">
-  <p>&copy; ShadesOfGreen - Created for educational purposes</p>
-</footer>
+    <footer class="mt-4 text-center">
+      <p>&copy; ShadesOfGreen - Created for educational purposes</p>
+    </footer>
 
-<script src="./node_modules/jquery/dist/jquery.min.js"></script>
-<script src="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-<script src="./node_modules/lightbox2/dist/js/lightbox.min.js"></script>
-</body>
+    <script src="./node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="./node_modules/lightbox2/dist/js/lightbox.min.js"></script>
+  </body>
 </html>
